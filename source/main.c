22da2e20d5c65c127a13a94e933603bc8aec1e5d
Vincent Huang <vhuan025@ucr.edu>
2021-05-02 22:10:23 -0700

    first commit

---
+++ source/main.c
@@ -0,0 +1,27 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	if (~PINA & 0x01)
+		PORTB = 0x0F;
+	else 
+		PORTB = 0x00;
+    }
+    return 1;
+}
